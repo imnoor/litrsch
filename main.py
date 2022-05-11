54880bd73d7049bebe6bb5752b4807d7efeb81e1
Yaseen <imheart@gmail.com>
2022-05-11 14:47:27 +0300

    main updaated

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+import sys
+sys.path.append('../src/')
+
 from bib_processor import BiBProcessor
 import ml_lit
 
 processor = BiBProcessor('../bibtex_collection.bib')
-processor.filter_abstract()
-
-tags = ['Enterprise Architecture']
-
-processor.filter_tag(tags)
-
+processor.preparser()
+tags = processor.get_tags() 
+      
 filter_list = ["ARTIFICIAL","INTELLIGENCE", "AI", "MACHINE LEARNING","AUTOMATION", "AUTONOMOUS","COGNITIVE","(AI)"]
-noise_list = ["RESEARCH", "NEW","STUDY","INFORMATION","CHANGE","TIME","BASED","LEVEL","ERA","MAIN", "INTRODUCTION","USE", "NEED",
-             "AIM", "EFFORTS","CONCEPTS","BIG","BOLD","BASIS","HELP", "PRACTICES", "CONTEXT","CREATED","EANBLE", "CASE","EVENTS","CREATION", "PROVIDED", "HIGHLY",
-             "INCLUDING", "ENABLED","ENABLE", "LEVELS", "LASTLY","UNITS", "ENHANCED", "BEST","LASTLY","DRAWN","OBJECTIVE","THESIS","KNOWN"]
+filename = "noise.txt"
+noise_list = []
+with open(filename) as f:
+    for line in f:
+        noise_list +=line.replace("\n","").split(",")
+noise_set = set(noise_list)
 
-theme_data = processor.get_themes(filter_list,50, noise_list=noise_list)
-print(theme_data['common'])
+for tag in tags:
+    print(tag)
+    processor.filter_tag([tag])
+    theme_data = processor.get_themes(filter_list,50, noise_list=noise_set)
+    print(theme_data['common'])
+
